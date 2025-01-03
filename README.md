ab9475b042ecb3ab513fc9f89c822df16658fb0f
pbuk <pb@pbuk.uk>
2025-01-03 00:47:56 +0000

    Starting ffmpeg

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # webcam-node
+
 Node JS code for downloading videos from the Minima Yacht Club webcam, processing and uploading to storage.
---
+++ scripts/ff.js
@@ -0,0 +1,141 @@
+import { readdir } from 'node:fs/promises';
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+
+import ffprobeStatic from 'ffprobe-static';
+import ffmpegStatic from 'ffmpeg-static';
+import ffmpeg from 'fluent-ffmpeg';
+
+const timer = performance.now();
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const downloadDir = path.join(__dirname, '..', 'files', 'download');
+
+const files = await readdir(downloadDir);
+// for (const file of files)
+//  	console.log(file, path.join(downloadDir, file));
+
+
+// Tell fluent-ffmpeg where it can find FFmpeg
+ffmpeg.setFfmpegPath(ffmpegStatic);
+ffmpeg.setFfprobePath(ffprobeStatic.path);
+
+const ff = ffmpeg();
+
+const scale = 1 / 180;
+
+for (const file of files) {
+	const filePath = path.join(downloadDir, file)
+	ff.addInput(filePath);
+	ff.addInputOptions([`-itsscale ${scale}`]);
+}
+	/*
+	.screenshots({
+		timestamps: [0],
+		filename: '%b.jpg',
+		// folder: '/path/to/output',
+		// size: '320x240'
+	})
+	*/
+
+	ff
+	.outputOptions('-metadata', 'title=Minima Yacht Club')
+	.outputOptions('-metadata', 'composer=')
+	.withFPS(30)
+	.noAudio()
+	.mergeToFile('output2.mp4', './files/tmp')
+  // .saveToFile('output.mp4')
+
+	// Log the percentage of work completed
+	.on("progress", (progress) => {
+		if (progress.percent) {
+			console.log(`Processing: ${(progress.percent)}% done`);
+		}
+	})
+
+	// The callback that is run when FFmpeg is finished
+	.on("end", () => {
+		console.log("FFmpeg has finished.");
+		console.log(performance.now() - timer);
+	})
+
+	// The callback that is run when FFmpeg encountered an error
+	.on("error", (error) => {
+		console.log("An error occurred: " + error.message);
+	});
+
+/*
+
+
+minimayc_2024-12-31_19-44-38.mp4 metadata
+{
+  streams: [
+    {
+      index: 0,
+      codec_name: 'h264',
+      codec_long_name: 'H.264 / AVC / MPEG-4 AVC / MPEG-4 part 10',
+      profile: 'Main',
+      codec_type: 'video',
+      codec_time_base: '30103/900000',
+      codec_tag_string: 'avc1',
+      codec_tag: '0x31637661',
+      width: 1920,
+      height: 1080,
+      coded_width: 1920,
+      coded_height: 1088,
+      has_b_frames: 0,
+      sample_aspect_ratio: 'N/A',
+      display_aspect_ratio: 'N/A',
+      pix_fmt: 'yuvj420p',
+      level: 40,
+      color_range: 'pc',
+      color_space: 'bt709',
+      color_transfer: 'bt709',
+      color_primaries: 'bt709',
+      chroma_location: 'left',
+      field_order: 'unknown',
+      timecode: 'N/A',
+      refs: 1,
+      is_avc: 'true',
+      nal_length_size: 4,
+      id: 'N/A',
+      r_frame_rate: '100/1',
+      avg_frame_rate: '450000/30103',
+      time_base: '1/90000',
+      start_pts: 0,
+      start_time: 0,
+      duration_ts: 27092700,
+      duration: 301.03,
+      bit_rate: 3135097,
+      max_bit_rate: 'N/A',
+      bits_per_raw_sample: 8,
+      nb_frames: 4500,
+      nb_read_frames: 'N/A',
+      nb_read_packets: 'N/A',
+      tags: [Object],
+      disposition: [Object]
+    }
+  ],
+  format: {
+    filename: 'C:\\Projects\\Maintained\\Minima\\webcam-node\\files\\download\\minimayc_2024-12-31_19-44-38.mp4',
+    nb_streams: 1,
+    nb_programs: 0,
+    format_name: 'mov,mp4,m4a,3gp,3g2,mj2',
+    format_long_name: 'QuickTime / MOV',
+    start_time: 0,
+    duration: 301.03,
+    size: 118015242,
+    bit_rate: 3136305,
+    probe_score: 100,
+    tags: {
+      major_brand: 'mp42',
+      minor_version: '0',
+      compatible_brands: 'mp42isom',
+      creation_time: '2024-12-31T19:44:38.000000Z',
+      title: 'Archived with Unreal Media',
+      composer: 'Unreal Archival Server'
+    }
+  },
+  chapters: []
+}
+*/
